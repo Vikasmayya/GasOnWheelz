e76d4445680bf341b7dd5e3dc88873c276e0c295
Vikasmayya <65965704+Vikasmayya@users.noreply.github.com>
2023-11-30 16:53:07 +0530

    configured github actions

---
+++ build/static/js/27.93ba20c0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgasonwheelz=this.webpackJsonpgasonwheelz||[]).push([[27],{630:function(e,t,s){"use strict";s.r(t);s(8);var n=s(9);t.default=function(){return Object(n.jsx)(n.Fragment,{children:Object(n.jsx)("footer",{className:"bg-light text-center",style:{marginTop:"5rem"},children:Object(n.jsx)("p",{children:"  \xa9 2021 Gas On Wheelz. All rights Reserved"})})})}}}]);
+//# sourceMappingURL=27.93ba20c0.chunk.js.map
